8fb03b1161d9aa4ee9db955f0f78589ab086b448
Moin <allmoinahmed@gmail.com>
2022-04-28 02:09:40 +0600

    DOM in practice

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log(document);
+let val = document;
+val = document.head
+val = document.body
+val = document.all
+val = document.all[5]
+val = document.title
+val = document.links[0]
+val = document.characterSet
+val = document.forms[0]
+console.log(val);
