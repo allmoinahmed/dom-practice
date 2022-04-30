edfa48c1267720976a98246a92c2c0b0b23eaa13
Moin <allmoinahmed@gmail.com>
2022-05-01 00:25:19 +0600

    Updated Dom

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 // Selecting Element
 const h1Elm = document.querySelector('h1')
 // const containerElm = document.querySelector('.container')
-// const navElm = document.querySelector('#nav')
+const navElm = document.querySelector('#nav')
 // const FirstElm = document.querySelector('#nav .first')
 // console.log(FirstElm);
 
@@ -24,6 +24,74 @@ const h1Elm = document.querySelector('h1')
 
 // reading elements
 // h1Elm.textContent = 'Our new <em>content</em>'
-h1Elm.innerHTML = 'Our new <em>content</em>'
+// h1Elm.innerHTML = 'Our new <em>content</em>'
 
-console.log(h1Elm.textContent);
+// console.log(h1Elm.textContent);
+
+
+// Traversing elements
+
+const firstNavElm = navElm.children[0];
+// console.log(firstNavElm.closest('.body'));
+
+// const NavArray = Array.from(navElm.children)
+// NavArray.forEach( items => console.log(items))
+
+
+// Removing elements
+//using target element
+// firstNavElm.remove()
+
+// Creating Element
+// <li class="first">First</li>
+const liElem = document.createElement('li')
+liElem.className = 'five'
+liElem.textContent = 'Five'
+// navElm.appendChild(liElem)
+// navElm.prepend(liElem)
+
+
+// insertAdjacentHTML
+// insertAdjacentElement
+
+navElm.insertAdjacentElement('beforeend', liElem)
+navElm.insertAdjacentHTML('afterbegin', '<em>After Begin</em>')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
