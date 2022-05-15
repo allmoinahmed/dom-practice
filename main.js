22329c527ec1b3a698f67975948938bd9f01c6a2
Moin <allmoinahmed@gmail.com>
2022-05-15 15:34:53 +0600

    updating traversing-dom

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-// Manipulation
-val = document.querySelector('.product-collection-item');
+let val = document.querySelector('.product-collection')
+// console.log(val);
 
+let lists = val.children[0].nextElementSibling.parentElement
+console.log(lists);
 
-// val.style.color = 'red'
-// val.style.backgroundColor = 'black'
-// val.textContent = 'Microphone'
-// val.innerText = 'Shirt'
-// val.innerHTML = '<em>Microphone</em>'
-// console.log(val.textContent);
-// val = document.querySelector("a").setAttribute('href', 'https://facebook.com')
-
-// val = val.getAttribute('class')
-// val.className = 'product-my-class'
-val.classList.add('my-class')
-
-console.log(val);
-
-// Selection
