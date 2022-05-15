163ccfca0f88e81d8e4764e49eee0829662d8c65
Moin <allmoinahmed@gmail.com>
2022-05-15 17:59:24 +0600

    Updating Creating-Element

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-// Interacting-and-child-Nodes
-// // 1 - Element
-// // 2 - Attribute (deprecated)
-// // 3 - Text node
-// // 4 - Comment
-// // 5 - document itself
-// // 6 - Doctype
- 
-let val = document.querySelector('.product-collection')
-// val = val.childNodes[1].nodeType
-if (val.childNodes[1].nodeType !== 3){
-    val.childNodes[1].classList.add('MyClass')
-}
-console.log(val);
+// Creating Multi Level UL LI
+let ulOne = document.querySelector('.product-collection')
+
+let liOne = document.createElement('li')
+liOne.className = 'product-collection-item'
+liOne.appendChild(document.createTextNode('My New Element'))
+ulOne.appendChild(liOne)
+
+liOne.classList.add('lastLi')
+
+let ulTwo = document.createElement('ul')
+ulTwo.className = 'product-collection-two'
+
+liOne.appendChild(ulTwo)
+
+let liTwo = document.createElement('li')
+liTwo.className = 'product-collection-item'
+liTwo.appendChild(document.createTextNode('My Second Items'))
+
+ulTwo.appendChild(liTwo)
+
+
