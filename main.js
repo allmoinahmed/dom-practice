010459c8ae59c293e6e702b8f95c7617e63799de
Moin <allmoinahmed@gmail.com>
2022-05-15 15:56:23 +0600

    updating Interacting-and-child-Nodes

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+// Interacting-and-child-Nodes
+// // 1 - Element
+// // 2 - Attribute (deprecated)
+// // 3 - Text node
+// // 4 - Comment
+// // 5 - document itself
+// // 6 - Doctype
+ 
 let val = document.querySelector('.product-collection')
-// console.log(val);
-
-let lists = val.children[0].nextElementSibling.parentElement
-console.log(lists);
-
+// val = val.childNodes[1].nodeType
+if (val.childNodes[1].nodeType !== 3){
+    val.childNodes[1].classList.add('MyClass')
+}
+console.log(val);
