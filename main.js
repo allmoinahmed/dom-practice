b7daa578a1799f5e70f84b911c79088021717809
Moin <allmoinahmed@gmail.com>
2022-05-15 09:51:19 +0600

    Updated Selecting Element

--- main.js
+++ main.js
@@ -5,17 +5,50 @@
 // const BodyElm = document.querySelectorAll('.product-collection-item')
 
 
-let value = document.getElementsByClassName('product-collection-item')
-let val = Array(value)
-// console.log(val);
+// let value = document.getElementsByClassName('product-collection-item')
+// let val = Array(value)
+// // console.log(val);
 
-// val.forEach(element => {
-//    console.log(node); 
-// });
+// // val.forEach(element => {
+// //    console.log(node); 
+// // });
 
-for(let i = 0; i < val.length; i++) {
-    console.log(val[i]);
-}
-for (const items of val) {
+// for(let i = 0; i < val.length; i++) {
+//     console.log(val[i]);
+// }
+// for (const items of val) {
+//     console.log(items);
+// }
+
+
+let val = document.getElementsByClassName('product-collection-item')
+
+// val = Array(val);
+
+// val.forEach(item => {
+//     console.log(item)
+// })
+
+// for (let items of val) {
+//     console.log(items);
+// }
+
+
+let val2 = document.querySelectorAll('.product-collection-item')
+
+// val = Array(val);
+
+val2.forEach(item => {
+    console.log(item)
+})
+
+for (let items of val2) {
     console.log(items);
 }
+
+
+
+
+
+
+
