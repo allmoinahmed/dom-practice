6b170a7dae2432aaa9eb1c403d84444644fe056c
Moin <allmoinahmed@gmail.com>
2022-05-15 10:05:44 +0600

    manipulating element update

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 // Manipulation
+val = document.querySelector('.product-collection-item');
 
+
+// val.style.color = 'red'
+// val.style.backgroundColor = 'black'
+// val.textContent = 'Microphone'
+// val.innerText = 'Shirt'
+// val.innerHTML = '<em>Microphone</em>'
+// console.log(val.textContent);
+// val = document.querySelector("a").setAttribute('href', 'https://facebook.com')
+
+// val = val.getAttribute('class')
+// val.className = 'product-my-class'
+val.classList.add('my-class')
+
+console.log(val);
 
 // Selection
