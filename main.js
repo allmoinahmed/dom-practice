78a8a65c1689baf617afb1d0b7d0b85e01015fe4
Moin <allmoinahmed@gmail.com>
2022-05-22 20:26:16 +0600

    Update All

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const h1 = document.querySelector('h1')
-h1.addEventListener('click', function(event) {
-    console.log(event)
-    console.log(event.target.innerText )
-    console.log('Click H1');
+const h1 = document.querySelector('.h1') 
+const li = document.querySelectorAll('li') 
+const inputEle = document.getElementById('productname') 
+const forms = document.forms[0] 
+function eventFunc(evt){
+    evt.preventDefault()
+    console.log(evt);
+    console.log('Type: ' + evt.type);
+    console.log('Value: ' + inputEle.value);
+
+}
+h1.addEventListener("click", eventFunc)
+
+li.forEach(items => {
+    items.addEventListener("click", eventFunc)
 })
 
-
-
-
+forms.addEventListener("submit", eventFunc)
